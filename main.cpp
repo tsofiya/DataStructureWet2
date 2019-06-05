7d0685bee31d1dda5ca790bff2dc66c856d07d93
tsofiya <47355819+tsofiya@users.noreply.github.com>
2019-06-05 18:45:45 +0300

    add git ignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include "AVLRankCounter.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Creating rank tree" << std::endl;
+    AVLRankCounter<int> tree;
+    tree.insert(8);
+    tree.insert(13);
+    tree.insert(7);
+    tree.insert(6);
+    tree.insert(3);
+    tree.insert(4);
+    tree.insert(25);
+    tree.insert(25);
+    tree.insert(3);
+    tree.insert(17);
+    tree.insert(16);
+    tree.insert(21);
+    tree.insert(21);
+    tree.insert(18);
+    tree.insert(9);
+    tree.insert(11);
+    tree.inOrderPrint();
+
+    tree.remove(3);
+    tree.remove(21);
+    tree.remove(9);
+    tree.inOrderPrint();
+
     return 0;
 }
